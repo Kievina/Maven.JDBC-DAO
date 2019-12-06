d3cdf03a759eed5423f5fd2cc8485026c57bfd91
Kievina <56592501+Kievina@users.noreply.github.com>
2019-12-06 17:50:59 -0500

    add car model tests

---
+++ src/test/java/models/CarDTOTest.java
@@ -0,0 +1,66 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarDTOTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getMake() {
+    }
+
+    @Test
+    public void setMake() {
+    }
+
+    @Test
+    public void getModel() {
+    }
+
+    @Test
+    public void setModel() {
+    }
+
+    @Test
+    public void getYear() {
+    }
+
+    @Test
+    public void setYear() {
+    }
+
+    @Test
+    public void getColor() {
+    }
+
+    @Test
+    public void setColor() {
+    }
+
+    @Test
+    public void getVin() {
+    }
+
+    @Test
+    public void setVin() {
+    }
+}
